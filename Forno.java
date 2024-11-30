0afd90df07e2efb35296b0b354e51f41b7a671db
mazz <lory.mazzoli0906@gmail.com>
2024-11-30 12:39:53 +0100

    Update Forno.java

--- Forno.java
+++ Forno.java
@@ -1,6 +1,6 @@
 public class Forno {
     private static final int CAPACITA_FORNO = 400;
-
+    
     public void cuociMattoni(int mattoni) throws InterruptedException {
         System.out.println("Forno: inizio cottura di " + mattoni + " mattoni.");
         Logger.scriviLog("Forno: inizio cottura di " + mattoni + " mattoni.");
